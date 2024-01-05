3c64c0eb50c2c3b91f5385746159226572483ba2
Yuri-SYV <steshenko.syv@gmail.com>
2024-01-05 21:40:24 +0300

    P0031 FirstProject

---
+++ p0041basicviews/src/main/java/com/example/p0041basicviews/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.p0041basicviews;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
