7884d09c6a76fa066abd493f7f137c3bc15d1d88
Yuri-SYV <steshenko.syv@gmail.com>
2024-01-06 19:24:40 +0300

    P0051 Layout Files

---
+++ p0051layoutfiles/src/main/java/com/example/p0051layoutfiles/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.p0051layoutfiles;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscreen);
+    }
+}
